b7f7530fbf1d3fdd1ade2c64188c42c7834647e3
Erin <erinabduli7@gmail.com>
2025-04-21 21:14:34 +0200

    fixed bug where the profile page wouldn't load for doctors because of approval issue

--- client/src/dashboard/doctor-account/Dashboard.jsx
+++ client/src/dashboard/doctor-account/Dashboard.jsx
@@ -21,7 +21,7 @@ const Dashboard = () => {
 					<div className="grid lg:grid-cols-3 gap-[30px] lg:gap-[50px]">
 						<Tabs tab={tab} setTab={setTab} />
 						<div className="lg:col-span-2">
-							{data.doctorData.isApproved === "pending" && (
+							{data?.doctorData?.isApproved === "pending" && (
 								<div className="flex p-4 mb-4 text-yellow-800 bg-yellow-50 rounded-lg">
 									<svg
 										className="flex-shrink-0 w-5 h-5"
@@ -44,6 +44,40 @@ const Dashboard = () => {
 									</div>
 								</div>
 							)}
+							<div className="mt-8">
+								{tab === "overview" && (
+									<div>
+										<div className="flex items-center gap-4 mb-10">
+											<figure className="max-w-[200px] max-h-[200px]">
+												<img
+													src={
+														data?.doctorData?.photo
+													}
+													alt=""
+													className="w-full"
+												/>
+											</figure>
+											<div>
+												<span className="bg-[#ccf0f3] text-irisBlueColor py-1 px-4 lg:py-2 lg:px-6 rounded text-[12px] leading-4 lg:text-[16px] lg:leading-6 font-semibold">
+													{data?.doctorData
+														?.specalization
+														? data?.doctorData
+																?.specalization
+														: "No Specialization"}
+												</span>
+
+												<h3 className="text-[22px] leading-9 font-bold text0gray-900 mt-3">
+													{data?.doctorData.name}
+												</h3>
+											</div>
+										</div>
+									</div>
+								)}
+								{tab === "appointments" && (
+									<div>appointments</div>
+								)}
+								{tab === "profile" && <div>profile</div>}
+							</div>
 						</div>
 					</div>
 				)}
