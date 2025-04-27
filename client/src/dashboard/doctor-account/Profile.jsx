8c03d946af813d6eed62dadbeb4e329148183998
ErinAbduli <erinabduli7@gmail.com>
2025-04-27 23:20:43 +0200

    continued the profile section

--- client/src/dashboard/doctor-account/Profile.jsx
+++ client/src/dashboard/doctor-account/Profile.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { AiOutlineDelete } from "react-icons/ai";
 
 const Profile = () => {
 	const [formData, setFormData] = useState({
@@ -8,7 +9,14 @@ const Profile = () => {
 		bio: "",
 		gender: "",
 		specialization: "",
-		ticketPrice: "",
+		ticketPrice: 0,
+		qualifications: [
+			{ startingDate: "", endingDate: "", degree: "", university: "" },
+		],
+		experiences: [
+			{ startingDate: "", endingDate: "", position: "", hospital: "" },
+		],
+		timeSlots: [{ day: "", startingTime: "", endingTime: "" }],
 	});
 
 	const handleInputChange = (e) => {
@@ -117,6 +125,198 @@ const Profile = () => {
 							/>
 						</div>
 					</div>
+				</div>
+				<div className="mb-5">
+					<p className="form__label">Qualifications*</p>
+					{formData.qualifications?.map((qualification, index) => (
+						<div key={index}>
+							<div>
+								<div className="grid grid-cols-2 gap-5">
+									<div>
+										<p className="form__label">
+											Starting Date*
+										</p>
+										<input
+											type="date"
+											name="startingDate"
+											value={qualification?.startingDate}
+											className="form__input"
+										/>
+									</div>
+									<div>
+										<p className="form__label">
+											Ending Date*
+										</p>
+										<input
+											type="date"
+											name="endingDate"
+											value={qualification?.endingDate}
+											className="form__input"
+										/>
+									</div>
+								</div>
+								<div className="grid grid-cols-2 gap-5 mt-5">
+									<div>
+										<p className="form__label">Degree*</p>
+										<input
+											type="text"
+											name="degree"
+											value={qualification?.degree}
+											className="form__input"
+										/>
+									</div>
+									<div>
+										<p className="form__label">
+											University*
+										</p>
+										<input
+											type="text"
+											name="university"
+											value={qualification?.university}
+											className="form__input"
+										/>
+									</div>
+								</div>
+								<button className="bg-red-600 p-2 rounded-full text-white text-[18px] mt-2 mb-[30px] cursor-pointer">
+									<AiOutlineDelete />
+								</button>
+							</div>
+						</div>
+					))}
+					<button className="bg-[#000] py-2 px-5 rounded text-white h-fit cursor-pointer">
+						Add Qualifications
+					</button>
+				</div>
+				<div className="mb-5">
+					<p className="form__label">Experiences*</p>
+					{formData.experiences?.map((experience, index) => (
+						<div key={index}>
+							<div>
+								<div className="grid grid-cols-2 gap-5">
+									<div>
+										<p className="form__label">
+											Starting Date*
+										</p>
+										<input
+											type="date"
+											name="startingDate"
+											value={experience?.startingDate}
+											className="form__input"
+										/>
+									</div>
+									<div>
+										<p className="form__label">
+											Ending Date*
+										</p>
+										<input
+											type="date"
+											name="endingDate"
+											value={experience?.endingDate}
+											className="form__input"
+										/>
+									</div>
+								</div>
+								<div className="grid grid-cols-2 gap-5 mt-5">
+									<div>
+										<p className="form__label">Position*</p>
+										<input
+											type="text"
+											name="position"
+											value={experience?.position}
+											className="form__input"
+										/>
+									</div>
+									<div>
+										<p className="form__label">Hospital*</p>
+										<input
+											type="text"
+											name="hospital"
+											value={experience?.hospital}
+											className="form__input"
+										/>
+									</div>
+								</div>
+								<button className="bg-red-600 p-2 rounded-full text-white text-[18px] mt-2 mb-[30px] cursor-pointer">
+									<AiOutlineDelete />
+								</button>
+							</div>
+						</div>
+					))}
+					<button className="bg-[#000] py-2 px-5 rounded text-white h-fit cursor-pointer">
+						Add Expereinces
+					</button>
+				</div>
+				<div className="mb-5">
+					<p className="form__label">Experiences*</p>
+					{formData.timeSlots?.map((timeSlot, index) => (
+						<div key={index}>
+							<div>
+								<div className="grid grid-cols-2 md:gird-cols-4 mb-[30px] gap-5">
+									<div>
+										<p className="form__label">Day*</p>
+										<select
+											name="day"
+											value={timeSlot.day}
+											className="form__input py-3.5"
+										>
+											<option value="">Select</option>
+											<option value="saturday">
+												Saturday
+											</option>
+											<option value="sunday">
+												Sunday
+											</option>
+											<option value="monday">
+												Monday
+											</option>
+											<option value="teusday">
+												Tuesday
+											</option>
+											<option value="wednesday">
+												Wednesday
+											</option>
+											<option value="thursday">
+												Thursday
+											</option>
+											<option value="friday">
+												Friday
+											</option>
+										</select>
+									</div>
+									<div>
+										<p className="form__label">
+											Starting Time*
+										</p>
+										<input
+											type="time"
+											name="startingTime"
+											value={timeSlot?.startingTime}
+											className="form__input"
+										/>
+									</div>
+									<div>
+										<p className="form__label">
+											Ending Time*
+										</p>
+										<input
+											type="time"
+											name="endingTime"
+											value={timeSlot?.endingTime}
+											className="form__input"
+										/>
+									</div>
+									<div className="flex items-center">
+										<button className="bg-red-600 p-2 rounded-full text-white text-[18px] cursor-pointer mt-6">
+											<AiOutlineDelete />
+										</button>
+									</div>
+								</div>
+							</div>
+						</div>
+					))}
+					<button className="bg-[#000] py-2 px-5 rounded text-white h-fit cursor-pointer">
+						Add Time Slot
+					</button>
 				</div>
 			</form>
 		</div>
