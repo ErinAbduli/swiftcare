bf39dac5202d290ebd5845e68e49a81860b39a12
Erin <erinabduli7@gmail.com>
2025-05-03 01:37:14 +0200

    finsihing up functionality

--- client/src/components/Doctors/DoctorCard.jsx
+++ client/src/components/Doctors/DoctorCard.jsx
@@ -1,44 +1,52 @@
-import React from 'react'
-import starIcon from '../../assets/images/Star.png'
-import { BsArrowRight } from 'react-icons/bs'
-import { Link } from 'react-router-dom'
+import React from "react";
+import starIcon from "../../assets/images/Star.png";
+import { BsArrowRight } from "react-icons/bs";
+import { Link } from "react-router-dom";
 const DoctorCard = ({ doctor }) => {
-    return (
-        <div className='p-3 lg:p-5'>
-            <div>
-                <img src={doctor.photo} className='w-full' alt="" />
-            </div>
-            <h2 className='text-[18px] leading-[30px] lg:text-[26px] lg:leading-9 text-gray-900 font-[700] mt-3 lg:mt-5'>
-                {doctor.name}
-            </h2>
-            <div className='mt-2 lg:mt-4 flex items-center justify-between'>
-                <span className='bg-[#ccf0f3] text-cyan-500 py-1 px-2 lg:py-2 lg:px-6 text-[12px] rounded leading-4 lg:text-[16px] lg:leading-7 font-semibold'>
-                    {doctor.speciality}
-                </span>
-                <div className='flex items-center hap-[6px]'>
-                    <span className='flex itmes-center justify-center gap-[6px] leading-6 lg:text-[16px] lg:leading-7 font-semibold text-gray-900'>
-                        <img className='h-6 w-6' src={starIcon} alt="" /> {doctor.avgRating}
-                    </span>
-                    <span className='flex itmes-center gap-[6px] leading-6 lg:text-[16px] lg:leading-7 font-[400] text-gray-600'>
-                        ({doctor.totalRating})
-                    </span>
-                </div>
-            </div>
-            <div className='mt-[18px] lg:mt-5 flex items-center justify-between'>
-                <div>
-                    <h3 className='text-[16px] leading-7 lg:text-[18px] lg:leading-[30px] font-semibold text-gray-900 '>
+	console.log(doctor);
+	return (
+		<div className="p-3 lg:p-5">
+			<div>
+				<img src={doctor.photo} className="w-full" alt="" />
+			</div>
+			<h2 className="text-[18px] leading-[30px] lg:text-[26px] lg:leading-9 text-gray-900 font-[700] mt-3 lg:mt-5">
+				{doctor.name}
+			</h2>
+			<div className="mt-2 lg:mt-4 flex items-center justify-between">
+				<span className="bg-[#ccf0f3] text-cyan-500 py-1 px-2 lg:py-2 lg:px-6 text-[12px] rounded leading-4 lg:text-[16px] lg:leading-7 font-semibold">
+					{doctor.specialization}
+				</span>
+				<div className="flex items-center hap-[6px]">
+					<span className="flex itmes-center justify-center gap-[6px] leading-6 lg:text-[16px] lg:leading-7 font-semibold text-gray-900">
+						<img className="h-6 w-6" src={starIcon} alt="" />{" "}
+						{doctor.avgRating}
+					</span>
+					<span className="flex itmes-center gap-[6px] leading-6 lg:text-[16px] lg:leading-7 font-[400] text-gray-600">
+						({doctor.totalRating})
+					</span>
+				</div>
+			</div>
+			<div className="mt-[18px] lg:mt-5 flex items-center justify-between">
+				<div>
+					{/* <h3 className='text-[16px] leading-7 lg:text-[18px] lg:leading-[30px] font-semibold text-gray-900 '>
                         {doctor.totalPatients}+ patients
-                    </h3>
-                    <p className='tezt-[14px] leading-6 font-[400] text-gray-600'>
-                        At {doctor.hospital}
-                    </p>
-                </div>
-                <Link to='/doctors' className='w-[44px] h-[44px] rounded-full border border-solid border-[#181a1e] flex items-center justify-center group hover:bg-blue-600 hover:border-none '>
-                    <BsArrowRight className='group-hover:text-white w-6 h-5' />
-                </Link>
-            </div>
-        </div>
-    )
-}
+                    </h3> */}
+					<p className="tezt-[14px] leading-6 font-[400] text-gray-600">
+						At{" "}
+						{doctor.experiences
+							? doctor?.experiences[0]?.hospital
+							: "N/A"}
+					</p>
+				</div>
+				<Link
+					to={`/doctors/${doctor._id}`}
+					className="w-[44px] h-[44px] rounded-full border border-solid border-[#181a1e] flex items-center justify-center group hover:bg-blue-600 hover:border-none "
+				>
+					<BsArrowRight className="group-hover:text-white w-6 h-5" />
+				</Link>
+			</div>
+		</div>
+	);
+};
 
-export default DoctorCard
+export default DoctorCard;
--- client/src/pages/Doctors/Doctors.jsx
+++ client/src/pages/Doctors/Doctors.jsx
@@ -1,10 +1,36 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import DoctorCard from "../../components/Doctors/DoctorCard";
 import { doctors } from "../../assets/data/doctors";
 import { BsSignIntersection } from "react-icons/bs";
 import Testimonial from "../../components/Testimonial/Testimonial";
+import { BASE_URL } from "../../config.js";
+import useFetchData from "../../hooks/useFetchData.jsx";
+import Loader from "../../components/Loader/Loading.jsx";
+import Error from "../../components/Error/Error.jsx";
 
 const Doctors = () => {
+	const [query, setQuery] = useState("");
+	const [debounceQuery, setDebounceQuery] = useState("");
+	const {
+		data: doctors,
+		loading,
+		error,
+	} = useFetchData(`${BASE_URL}/doctors?query=${debounceQuery}`);
+
+	const handleSearch = () => {
+		setQuery(query.trim());
+	};
+
+	useEffect(() => {
+		const timeout = setTimeout(() => {
+			setDebounceQuery(query);
+		}, 700);
+
+		return () => {
+			clearTimeout(timeout);
+		};
+	}, [query]);
+
 	return (
 		<>
 			<section className="bg-[#fff9ea]">
@@ -12,11 +38,16 @@ const Doctors = () => {
 					<h2 className="heading">Find a Doctor</h2>
 					<div className="max-w-[570px] mt-[30px] mx-auto bg-[#0066ff2c] rounded-md flex items-center justify-between">
 						<input
+							value={query}
+							onChange={(e) => setQuery(e.target.value)}
 							type="text"
-							placeholder="Search Doctor"
+							placeholder="Search doctor by name or specialty"
 							className="py-4 pl-4 pr-2 bg-transparent w-full focus:outline-none cursor-pointer placeholder:text-gray-600"
 						/>
-						<button className="btn mt-0 rounded-[0px] rounded-r-md">
+						<button
+							className="btn mt-0 rounded-[0px] rounded-r-md"
+							onClick={handleSearch}
+						>
 							Search
 						</button>
 					</div>
@@ -24,11 +55,15 @@ const Doctors = () => {
 			</section>
 			<section>
 				<div className="container">
-					<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
-						{doctors.map((doctor) => (
-							<DoctorCard key={doctor.id} doctor={doctor} />
-						))}
-					</div>
+					{loading && <Loader />}
+					{error && <Error />}
+					{!loading && !error && (
+						<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+							{doctors.map((doctor) => (
+								<DoctorCard key={doctor._id} doctor={doctor} />
+							))}
+						</div>
+					)}
 				</div>
 			</section>
 			<section>
